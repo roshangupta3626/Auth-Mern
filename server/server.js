d7a20bc7310b653d2dd1423a5751a84eee77d3f3
Roshan Gupta <rg9885468@gmail.com>
2026-03-13 21:39:52 +0530

    fix: cors trailing slash issue

--- server/server.js
+++ server/server.js
@@ -3,24 +3,27 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/mongodb.js';
-
 import authRouter from './routes/authRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
-
-
 const app = express();
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-   origin: process.env.CLIENT_URL,
+  origin: function(origin, callback) {
+    const allowedOrigin = process.env.CLIENT_URL?.replace(/\/$/, '');
+    if (!origin || origin.replace(/\/$/, '') === allowedOrigin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
-
 
 app.get('/', (req, res) => res.send('API is running... fine Roshan Gupta'));
 app.use('/api/auth', authRouter);
